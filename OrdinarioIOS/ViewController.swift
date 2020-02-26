0acda67bf1a01c30875ed533db4895601a80e7ab
Alejandro Sanz Muñoz <alejandro.sanz@utmac0018.local>
2020-02-26 11:13:26 +0100

    Initial Commit

---
+++ OrdinarioIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrdinarioIOS
+//
+//  Created by Alejandro Sanz Muñoz on 26/2/20.
+//  Copyright © 2020 ALEJANDRO SANZ MUÑOZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
